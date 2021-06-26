43dec2574eebf6ceb2270b280eb83c5ea3a8b440
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-06-26 21:12:13 +0000

    using recrusiong to sum numbers from a - b

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  let val = fromN
+  if (fromN === toN) {
+    return val
+  }
+  console.log(val)
+  return val + sum(fromN + 1, toN) // first loop
 }
 
+console.log(sum(3, 7))
 module.exports = sum;
+
+// 3, 4, 5, 6, 7 = 25
